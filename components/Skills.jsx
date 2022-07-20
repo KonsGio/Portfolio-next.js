6993d65c971bede36da2891e026940084490fad8
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-20 18:30:56 +0300

    Skills colors updated

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -5,9 +5,9 @@ import React from 'react';
 const Skills = () => {
 
     return (
-    <div className='w-full lg:h-screen p-2'>
+    <div id='#skills' className='w-full lg:h-screen p-2'>
     <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
-      <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+      <p className='uppercase text-xl tracking-widest text-[#6f3d01]'>
         Skills
       </p>
       <h2 className='py-4'>What I Can Do</h2>
