8453d15f63768d88eeae4eaea61ede3b35108ee8
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-25 14:06:31 +0300

    Tika Taka

--- pages/projects/tikataka.jsx
+++ pages/projects/tikataka.jsx
@@ -1,8 +1,80 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import tikapic from '../../public/assets/projects/tikataka.png';
+import { RiRadioButtonFill } from 'react-icons/ri';
+import Link from 'next/link';
 
 const tikataka = () => {
   return (
-    <div>tikataka</div>
+    <div className='w-full'>
+          <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+            <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/60 z-10' />
+            <Image
+              className='absolute z-1'
+              layout='fill'
+              objectFit='cover'
+              src={tikapic}
+              alt='/'
+            />
+            <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+              <h2 className='py-2'>Tika Taka</h2>
+              <h3>Next.js / Typescript / TailWind</h3>
+            </div>
+          </div>
+    
+          <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
+            <div className='col-span-4'>
+              <p>Project</p>
+              <h2>Overview</h2>
+              <p>
+              Tika Taka is an application inspired by tik-tok where you can log in with your Google account, upload videos and memes, and have lots of fun. Give it a go yourself!
+              </p>
+              <a
+                href='https://github.com/KonsGio/Tika-Taka'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+              </a>
+              <a
+                href='https://tika-taka.netlify.app'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <button className='px-8 py-2 mt-4'>Demo</button>
+              </a>
+    
+            </div>
+            <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+              <div className='p-2'>
+                <p className='text-center font-bold pb-2'>Technologies used</p>
+                <div className='grid grid-cols-3 md:grid-cols-1 '>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> React.js
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> Next.js
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> Google-Console
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> TailWind
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> Node.js
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> Typescript
+                  </p>
+                </div>
+              </div>
+            </div>
+            <Link href='/#projects'>
+              <p className='underline cursor-pointer'>Back</p>
+            </Link>
+          </div>
+        </div>
   )
 }
 
