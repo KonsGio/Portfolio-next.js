5f488f89f5b825dd11bf12fa3b5d0e3489f238ef
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-25 13:35:26 +0300

    Admin UI

--- pages/projects/devapp.jsx
+++ pages/projects/devapp.jsx
@@ -1,24 +1,24 @@
+import React from 'react'
 import Image from 'next/image';
-import React from 'react';
-import devchat from '../../public/assets/projects/dev-chat.png';
+import devapppic from '../../public/assets/projects/dev-app.png';
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
 const devapp = () => {
-    return (
-        <div className='w-full'>
-          <div className='w-screen h-[50vh] relative'>
-            <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+  return (
+    <div className='w-full'>
+          <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+            <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/60 z-10' />
             <Image
               className='absolute z-1'
               layout='fill'
               objectFit='cover'
-              src={devchat}
+              src={devapppic}
               alt='/'
             />
             <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-              <h2 className='py-2'>Crypto App</h2>
-              <h3>React JS / Tailwind / Firebase</h3>
+              <h2 className='py-2'>Admin UI Dashboard</h2>
+              <h3>React JS / Typescript / TailWind</h3>
             </div>
           </div>
     
@@ -27,17 +27,19 @@ const devapp = () => {
               <p>Project</p>
               <h2>Overview</h2>
               <p>
-                
+                This UI Dashboard application features a number of visual representations for income and outcome values and also general tools for simulating chart data. 
+                This project was built, to explore the ready components that <a href="https://www.syncfusion.com"><strong>Syncfusion </strong></a>provides.
+
               </p>
               <a
-                href='https://github.com/fireclint/crypto-react-firebase'
+                href='https://github.com/KonsGio/Admin-UI-Dashboard'
                 target='_blank'
                 rel='noreferrer'
               >
                 <button className='px-8 py-2 mt-4 mr-8'>Code</button>
               </a>
               <a
-                href='https://cryptobase-yt.web.app/'
+                href='https://kgio-dev-dashboard.netlify.app'
                 target='_blank'
                 rel='noreferrer'
               >
@@ -47,22 +49,22 @@ const devapp = () => {
             </div>
             <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
               <div className='p-2'>
-                <p className='text-center font-bold pb-2'>Technologies</p>
+                <p className='text-center font-bold pb-2'>Technologies used</p>
                 <div className='grid grid-cols-3 md:grid-cols-1 '>
                   <p className='text-gray-600 py-2 flex items-center'>
                     <RiRadioButtonFill className='pr-1' /> React
                   </p>
                   <p className='text-gray-600 py-2 flex items-center'>
-                    <RiRadioButtonFill className='pr-1' /> Tailwind
+                    <RiRadioButtonFill className='pr-1' /> Typescript
                   </p>
                   <p className='text-gray-600 py-2 flex items-center'>
-                    <RiRadioButtonFill className='pr-1' /> Javascript
+                    <RiRadioButtonFill className='pr-1' /> PostCSS
                   </p>
                   <p className='text-gray-600 py-2 flex items-center'>
-                    <RiRadioButtonFill className='pr-1' /> Firebase
+                    <RiRadioButtonFill className='pr-1' /> TailWind
                   </p>
                   <p className='text-gray-600 py-2 flex items-center'>
-                    <RiRadioButtonFill className='pr-1' /> Coin Gecko API
+                    <RiRadioButtonFill className='pr-1' /> Syncfusion
                   </p>
                   <p className='text-gray-600 py-2 flex items-center'>
                     <RiRadioButtonFill className='pr-1' /> Routes
@@ -75,7 +77,7 @@ const devapp = () => {
             </Link>
           </div>
         </div>
-      );
+  )
 }
 
 export default devapp
