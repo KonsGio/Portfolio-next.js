fb573f4f6c8b2a8d315bb713437edd0f5b523189
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-25 14:45:35 +0300

    Main updated links and colors

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {  AiOutlineMail  } from 'react-icons/ai';
 import { FaFileDownload, FaGithub, FaLinkedinIn } from 'react-icons/fa';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const Main = () => {
   return (
@@ -20,16 +19,16 @@ const Main = () => {
                 </p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                     <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
-                         <FaLinkedinIn />
+                        <a onClick={() => setNav(false)} href='https://www.linkedin.com/in/konstantinos-giovanitsas-10b511150/' target='_blank' className='text-[#6f3d01]'><FaLinkedinIn  /></a>
                     </div>
                     <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
-                         <FaGithub />
+                        <a onClick={() => setNav(false)} href='https://github.com/KonsGio' target='_blank' className='text-[#6f3d01]'><FaGithub /></a>
                     </div>
                     <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
-                         <AiOutlineMail />
+                        <a onClick={() => setNav(false)} href='/#contact' className='text-[#6f3d01]'><AiOutlineMail /></a>
                     </div>
                     <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
-                    <FaFileDownload/>
+                        <a onClick={() => setNav(false)} href='https://mega.nz/file/xYNlnSCI#B8-UZI987dsWx41SDiqhOHc9Sg0EBqUZKvHOe7P65Ig' target='_blank' className='text-[#6f3d01]'><FaFileDownload /></a>
                    </div>
                 </div>
             </div>
