ecf26bad58535f4eeb517ae628e2ce09328b164f
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-25 13:35:15 +0300

    Griekse Gemeenscap

--- pages/projects/gemenschaap.jsx
+++ pages/projects/gemenschaap.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import ellines from '../../public/assets/projects/ellines.png';
+import { RiRadioButtonFill } from 'react-icons/ri';
+import Link from 'next/link';
 
 const gemenschaap = () => {
   return (
-    <div>gemenschaap</div>
+    <div className='w-full'>
+          <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+            <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/60 z-10' />
+            <Image
+              className='absolute z-1'
+              layout='fill'
+              objectFit='cover'
+              src={ellines}
+              alt='/'
+            />
+            <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+              <h2 className='py-2'>Griekse Gemeenschap van Amsterdam</h2>
+              <h3>Wordpress / PHP / CSS</h3>
+            </div>
+          </div>
+    
+          <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
+            <div className='col-span-4'>
+              <p>Project</p>
+              <h2>Overview</h2>
+              <p>
+                Griekse Gemeenschap van Amsterdam, is the Greek community of Amsterdam and its surrounding areas. 
+              </p>
+              <p>
+                This website was built in the Wordpress environment, along with the Colibri template.
+                
+              </p>
+        
+              <a
+                href='https://ellines.nl'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <button className='px-8 py-2 mt-4'>Demo</button>
+              </a>
+    
+            </div>
+            <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+              <div className='p-2'>
+                <p className='text-center font-bold pb-2'>Technologies used</p>
+                <div className='grid grid-cols-3 md:grid-cols-1 '>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> Wordpress
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> PHP
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> CSS
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> Colibri
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> Yoast SEO
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> GDPR Cookie Consent
+
+                  </p>
+                </div>
+              </div>
+            </div>
+            <Link href='/#projects'>
+              <p className='underline cursor-pointer'>Back</p>
+            </Link>
+          </div>
+        </div>
   )
 }
 
