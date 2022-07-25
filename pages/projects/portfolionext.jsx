e10cc7276ba4ea2a54e9468537d8ccded30e5f22
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-25 16:03:16 +0300

    Portfolio next

--- pages/projects/portfolionext.jsx
+++ pages/projects/portfolionext.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
+import portfolionextpic from '../../public/assets/projects/portfolio-next.png';
+import { RiRadioButtonFill } from 'react-icons/ri';
+import Link from 'next/link';
+
 
 const portfolionext = () => {
   return (
-    <div>portfolionext</div>
+    <div className='w-full'>
+          <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+            <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/60 z-10' />
+            <Image
+              className='absolute z-1'
+              layout='fill'
+              objectFit='cover'
+              src={portfolionextpic}
+              alt='/'
+            />
+            <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+              <h2 className='py-2'>My portfolio in React</h2>
+              <h3>React / Next.js / TailWind</h3>
+            </div>
+          </div>
+    
+          <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
+            <div className='col-span-4'>
+              <p>Project</p>
+              <h2>Overview</h2>
+              <p>
+                This portfolio website is designed in React with next.js, it is the second portfolio in my projects and the better looking one. 
+              </p>
+              <a
+                href='https://github.com/KonsGio/Portfolio-next.js'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+              </a>
+              <a
+                href='https://www.kgio.dev'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <button className='px-8 py-2 mt-4'>Demo</button>
+              </a>
+    
+            </div>
+            <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+              <div className='p-2'>
+                <p className='text-center font-bold pb-2'>Technologies used</p>
+                <div className='grid grid-cols-3 md:grid-cols-1 '>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> React
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> Next.js
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> TailWind
+                  </p>
+                  <p className='text-gray-600 py-2 flex items-center'>
+                    <RiRadioButtonFill className='pr-1' /> Lazy loading images
+                  </p>
+                </div>
+              </div>
+            </div>
+            <Link href='/#projects'>
+              <p className='underline cursor-pointer'>Back</p>
+            </Link>
+          </div>
+        </div>
   )
 }
 
