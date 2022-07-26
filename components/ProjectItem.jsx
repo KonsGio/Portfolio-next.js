5d691b9e2b91c58563aee1a9f61dd95b350b0c5b
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-07-26 19:02:40 +0300

    Color change project item hover

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const ProjectItem = ({title, backgroundImg, projectUrl,tech}) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#6f3d01] to-[#ecc494]'>
+    <div className='relative flex items-center justify-center h-auto w-full shadow-xl mt-5 shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#875921] to-[#ecc494]'>
     <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/'/>
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
