5b8bcb733441dc606dee60033e610177aff77ae8
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-23 16:09:09 +0300

    Bug fix

--- pages/projects/bageri.jsx
+++ pages/projects/bageri.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
-import bageri from '../../public/assets/projects/bageri.png';
+import bageriimg from '../../public/assets/projects/bageri.png';
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
@@ -13,7 +13,7 @@ const bageri = () => {
               className='absolute z-1'
               layout='fill'
               objectFit='cover'
-              src={bageri}
+              src={bageriimg}
               alt='/'
             />
             <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
