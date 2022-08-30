ced4395138a2bcaca4ee1348975ba0e9440ab20b
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-30 22:16:11 +0300

    Changed CV and contact link

--- components/Main.jsx
+++ components/Main.jsx
@@ -27,10 +27,10 @@ const Main = () => {
                         <a href='https://github.com/KonsGio' rel="noreferrer" target='_blank' className='text-[#6f3d01]'><FaGithub /></a>
                     </div>
                     <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
-                        <a href='mailto:konstantinos.giovanitsas@yahoo.com' className='text-[#6f3d01]'><AiOutlineMail /></a>
+                        <a href='/#contact' className='text-[#6f3d01]'><AiOutlineMail /></a>
                     </div>
                     <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
-                        <a href='https://mega.nz/file/xYNlnSCI#B8-UZI987dsWx41SDiqhOHc9Sg0EBqUZKvHOe7P65Ig' rel="noreferrer" target='_blank' className='text-[#6f3d01]'><FaFileDownload /></a>
+                        <a href='https://mega.nz/file/ZVdDgThA#cVt8ME2hJh2UZfLZrLIIfvFh91rVZv9oN4E500mEzd8' rel="noreferrer" target='_blank' className='text-[#6f3d01]'><FaFileDownload /></a>
                    </div>
                 </div>
                 <div className='flex justify-center py-12'>
