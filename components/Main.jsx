c20ba5527fe0acbf23740f8f6212bdde2d65596e
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-11-25 18:04:12 +0200

    Main colors update

--- components/Main.jsx
+++ components/Main.jsx
@@ -14,28 +14,28 @@ const Main = () => {
                   Hello, I am <span className='text-[#6f3d01]'>Konstantinos.</span>
                 </h1>
                 <h2 className='py-4 text-gray-700'>
-                  A Front-End Web Developer
+                  Welcome to my portfolio
                 </h2>
                 <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
-                  I am a front-end developer specializing in designing and building web applications. Currently, I am focused on building responsive web applications, while learning back-end technologies.
+                  I am a web app developer and software engineer. Currently, I am focused on building web applications, while using different back-end technologies.
                 </p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
-                    <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
+                    <div className='rounded-full shadow-[#6f3d01a5] shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-[#ad7e44]'>
                         <a href='https://www.linkedin.com/in/konstantinos-giovanitsas-10b511150/' rel="noreferrer" target='_blank' className='text-[#6f3d01]'><FaLinkedinIn  /></a>
                     </div>
-                    <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
+                    <div className='rounded-full shadow-[#6f3d01a5] shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-[#ad7e44]'>
                         <a href='https://github.com/KonsGio' rel="noreferrer" target='_blank' className='text-[#6f3d01]'><FaGithub /></a>
                     </div>
-                    <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
+                    <div className='rounded-full shadow-[#6f3d01a5] shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-[#ad7e44]'>
                         <a href='mailto:konstantinos.giovanitsas@yahoo.com' className='text-[#6f3d01]'><AiOutlineMail /></a>
                     </div>
-                    <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
+                    <div className='rounded-full shadow-[#6f3d01a5] shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-[#ad7e44]'>
                         <a href='https://mega.nz/file/tN1ijTbb#Cy6-jmWSKQn_rbgcOq19FrvSJ_M98avaXaju55J_vYM' rel="noreferrer" target='_blank' className='text-[#6f3d01]'><FaFileDownload /></a>
                    </div>
                 </div>
                 <div className='flex justify-center py-12'>
-                    <Link href='/#skills'>
-                        <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
+                    <Link href='/#about'>
+                        <div className='rounded-full shadow-[#6f3d01a5] shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-[#ad7e44] animate-bounce'>
                             <HiOutlineChevronDoubleDown size={30} className='m-auto text-[#6f3d01]'/>
                         </div>
                     </Link>
