324fa958af7137b20479c955e86c7ccca2f5b41f
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-11-27 20:10:43 +0200

    gameenschaap update colors

--- pages/projects/gemenschaap.jsx
+++ pages/projects/gemenschaap.jsx
@@ -40,11 +40,11 @@ const gemenschaap = () => {
                 target='_blank'
                 rel='noreferrer'
               >
-                <button className='px-8 py-2 mt-4'>Demo</button>
+                <button className='px-8 py-2 mt-4 shadow-[#6f3d01a5] hover:shadow-[#ad7e44] hover:scale-110 ease-in duration-300'>Demo</button>
               </a>
     
             </div>
-            <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+            <div className='col-span-4 md:col-span-1 shadow-xl shadow-[#6f3d01a5] hover:shadow-[#ad7e44] hover:scale-110 ease-in duration-300 rounded-xl py-4'>
               <div className='p-2'>
                 <p className='text-center font-bold pb-2'>Technologies used</p>
                 <div className='grid grid-cols-3 md:grid-cols-1 '>
@@ -72,7 +72,7 @@ const gemenschaap = () => {
             </div>
             <div className='flex justify-center py-12'>
                     <Link href='/#projects'>
-                        <div className='rounded-full shadow-gray-400 shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 hover:shadow-gray-600'>
+                        <div className='rounded-full shadow-lg p-6 cursor-pointer hover:scale-110 ease-in duration-300 shadow-[#6f3d01a5] hover:shadow-[#ad7e44]'>
                             <HiOutlineChevronDoubleLeft size={30} className='m-auto text-[#6f3d01]'/>
                         </div>
                     </Link>
