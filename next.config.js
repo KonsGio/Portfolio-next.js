0553e0b4470863a3cbde9c5a3d74b2658f1c1fb3
KonsGio <konstantinos.giovanitsas@yahoo.com>
2023-01-12 21:25:23 +0100

    For nl

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   i18n: {
-    locales: ['en', 'nl'],
-    defaultLocale: 'en',
+    locales: ['en-US', 'nl-NL'],
+    defaultLocale: 'en-US',
   },
 }
 
